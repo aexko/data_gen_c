23ce313308c8efee9d079d3017120e62dd612531
Alex Kao <alexkxosb@gmail.com>
2023-10-13 18:02:57 -0400

    random char WORKS

--- generate.c
+++ generate.c
@@ -1,6 +1,7 @@
 #include "generate.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define PASSWORD_MAX_VALUE 16
@@ -23,47 +24,39 @@ char generate_first_name() {}
 // sin
 // password
 
-// to generate random int between range:
-// https://stackoverflow.com/questions/17846212/generate-a-random-number-between-1-and-10-in-c
 void generate_password() {
   srand(time(NULL));
   int random_password_length =
       rand() % ((PASSWORD_MAX_VALUE + 1) - PASSWORD_MIN_VALUE) +
       PASSWORD_MIN_VALUE;
   char password_generated[random_password_length];
+  printf("password length is %d\n", random_password_length);
 
   for (int i = 0; i < random_password_length; i++) {
 
-    //    password_generated[i] = ''
-    printf("%c\n", rand());
+    password_generated[i] = generate_random_char();
+    //    printf("%d\n", generate_random_number(0, 3));
   }
-  //  printf("%s", password_generated);
+  printf("password is %s\n", password_generated);
   //  return password_generated;
 }
 
 // to find total char in pointer
 // https://stackoverflow.com/questions/13551017/find-the-size-of-a-string-pointed-by-a-pointer
-char generate_random_character() {
-  char *alphabet_uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
-  char *lower_case = "qwertyuiopasdfghjklzxcvbnm";
-  char *special_characters = "~!@#$%^&*()_+{}:\"<>?";
+char generate_random_char() {
+  const char chars_authorized[] = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjk"
+                                  "lzxcvbnm~!@#$%^&*()_+{}:\"<>?./;'[]";
+  int chars_authorized_length =
+      sizeof(chars_authorized) / sizeof(chars_authorized[0]);
+  char random_char = generate_random_number(0, chars_authorized_length);
+  printf("%c\n", chars_authorized[random_char]);
+  return chars_authorized[random_char];
+}
 
-  
-
-
-
-  switch (rand() % 2) {
-  case ALPHABET_UPPERCASE:
-    break;
-
-  case ALPHABET_LOWERCASE:
-    break;
-
-  case SPECIAL_CHARACTERS:
-    break;
-
-  default:
-    // default not required
-    break;
-  }
+// to generate random int between range:
+// https://stackoverflow.com/questions/17846212/generate-a-random-number-between-1-and-10-in-c
+int generate_random_number(int min, int max) {
+  int random_number;
+  random_number = rand() % ((max + 1) - min) - min;
+  return random_number;
 }
--- generate.h
+++ generate.h
@@ -2,6 +2,8 @@
 #define A1_KAO_ALEX_40286533_GENERATE_H
 
 void generate_data();
+char generate_random_char();
+int generate_random_number(int min, int max);
 void generate_password();
 
 #endif
